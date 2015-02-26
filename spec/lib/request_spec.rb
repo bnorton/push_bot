39482db021369f73698954e3f96530a8b49f919a
Brian Norton <brian.nort@gmail.com>
2015-02-26 14:27:53 -0800

    Refactor request spec.

--- spec/lib/request_spec.rb
+++ spec/lib/request_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe PushBot::Request do
-  describe "#request" do
-    describe "POST" do
-      let(:id){42}
-      let(:secret){'super_secret_key'}
-      let(:type){:all}
-      let(:options){{msg:'foo',platform: '0'}}
-      let(:request_options){{
+  describe '#request' do
+    describe 'POST' do
+      let(:id) { 42 }
+      let(:secret) { 'super_secret_key' }
+      let(:type) { :all }
+      let(:options) { { msg:'foo', platform: '0' } }
+      let(:request_options) { {
         method: :post,
         body: JSON.dump(options),
         headers: {
@@ -15,14 +15,16 @@ describe PushBot::Request do
           :'X-PushBots-Secret' => secret,
           :'Content-Type' => :'application/json'
         }
-      }}
+      } }
+
       before do
         PushBot.configure do |config|
           config.id = id
           config.secret = secret
         end
       end
-      it "Typhoeus::Request should receive #new" do
+
+      it 'Typhoeus::Request should receive #new' do
         expect(Typhoeus::Request).to receive(:new).with("https://api.pushbots.com/push/#{type}",request_options)
         described_class.new(:push).post(type, options)
       end
